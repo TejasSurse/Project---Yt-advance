35c8d7e9dec82b1f88de2e58b7351182309124f2
Tejas Surse <tejas.s@porfyro.com>
2025-04-21 22:56:36 +0530

    Login and logout

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -162,8 +162,29 @@ const loginUser = asyncHandler(async(req, res)=>{
 
 
 const logOutUser = asyncHandler( async(req, res) =>{
-    
+    // now req. user ka accesss ka acces he humre pass
+    await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $set :{
+                refreshToken : undefined
+            }
+        },
+        {
+            new : true
+        }
+    );
+    const options = {
+        httpOnly : true,
+        secure : true
+    }
+
+    return res.
+    status(200).
+    clearCookie("accessToken", options).
+    clearCookie("refreshToken", options).
+    json( new ApiResponse(200, {}, "User Logout Successfully"));
 });
 
 
-export { registerUser, loginUser };
+export { registerUser, loginUser, logOutUser };
---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,27 @@
+import {asyncHandler} from "../utils/asyncHandler.js";
+import {ApiError} from "../utils/ApiError.js";
+import jwt from "jsonwebtoken";
+import {User} from "../models/user.model.js";
+export const verifyJWT = asyncHandler(async(req, res, next)=>{
+    // read jwt docs 
+    // tocken nikalo ya to cookies se ya to header se 
+    try{
+    const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ","");
+
+    if(!token){
+        throw new ApiError(401, "Unauthorized Request");
+    }
+ 
+    const decodeTocken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    
+    await User.findById(decodedToken?._id).select("-password -refreshToken");
+    if(!user){
+        // Todo next video
+        throw new ApiError(401, "Invalid Access Token");
+    }
+    req.user = user;
+    next();
+    } catch(error){
+        throw new ApiError(401, error?.message || "Invalid Access")
+    }   
+});
